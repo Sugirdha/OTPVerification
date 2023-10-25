66e33cd2fba1ce91f2e41bb7f43048cc2f750ddc
Sugirdha <sugirdha.v@gmail.com>
2023-10-25 22:36:30 +0800

    Initial Commit

---
+++ OTPVerification/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OTPVerification
+//
+//  Created by Sugirdha Vaithyanathan on 25/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
